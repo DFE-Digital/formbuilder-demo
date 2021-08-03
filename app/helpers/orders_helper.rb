b8a53ad9d49d49f3af58a7f6b844d797f418d258
Peter Yates <peter.yates@graphia.co.uk>
2021-08-03 13:07:41 +0100

    Render summary list row using a helper

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,8 @@
 module OrdersHelper
+  def summary_list_row_builder(row, key:, value:, actions: [])
+    row.key { key }
+    row.value { value }
+
+    actions.each { |name, path| row.action(href: path, text: name) }
+  end
 end
